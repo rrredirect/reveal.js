526c2bf860de3c51a0e40892d4798fe6b313cee9
rrredirect <58528557+rrredirect@users.noreply.github.com>
2025-03-12 09:53:23 -0400

    ready

---
+++ extra.php
@@ -0,0 +1,82 @@
+
+<!-- Slide 6: Same as the last but with no text (empty p) -->
+<section 
+data-background-image="http://example.com/image-for-slide6.png"
+data-background-size="cover"
+data-background-position="center">
+</section>
+
+<!-- Slide 7: Two <p> fragments -->
+<section 
+data-background-image="http://example.com/image-for-slide7.png"
+data-background-size="cover"
+data-background-position="center">
+
+<div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100%;">
+
+  <!-- Two fragments showing text -->
+  <p class="fragment" style="font-size: 2em; margin: 20px 0;">This is the first text fragment.</p>
+  <p class="fragment" style="font-size: 2em; margin: 20px 0;">Here's the second text fragment!</p>
+
+</div>
+
+</section>
+
+
+
+
+<!-- Slide 14: Image background -->
+<section 
+data-background-image="http://example.com/image-for-slide14.png"
+data-background-size="cover"
+data-background-position="center">
+</section>
+
+<!-- Slide 15: Image background -->
+<section 
+data-background-image="http://example.com/image-for-slide15.png"
+data-background-size="cover"
+data-background-position="center">
+</section>
+
+<!-- Slide 16: Image background with two fragments showing bullet points -->
+<section 
+data-background-image="http://example.com/image-for-slide16.png"
+data-background-size="cover"
+data-background-position="center">
+
+<div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100%;">
+
+  <p class="fragment" style="font-size: 2em; margin: 20px 0;">This is the first line of text!</p>
+
+  <ul>
+	<li class="fragment">First bullet point</li>
+	<li class="fragment">Second bullet point</li>
+	<li class="fragment">Third bullet point</li>
+	<li class="fragment">Fourth bullet point</li>
+  </ul>
+
+</div>
+
+</section>
+
+<!-- Slide 17: Image background with two fragments showing bullet points -->
+<section 
+data-background-image="http://example.com/image-for-slide17.png"
+data-background-size="cover"
+data-background-position="center">
+
+<div style="display: flex; flex-direction: column; align-items: center; justify-content: center; height: 100%;">
+
+  <p class="fragment" style="font-size: 2em; margin: 20px 0;">Another line of text!</p>
+
+  <ul>
+	<li class="fragment">Bullet point 1</li>
+	<li class="fragment">Bullet point 2</li>
+	<li class="fragment">Bullet point 3</li>
+	<li class="fragment">Bullet point 4</li>
+  </ul>
+
+</div>
+
+</section>
